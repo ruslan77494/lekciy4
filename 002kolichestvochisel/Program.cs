5b4a38fc1d40868ce0b1b53aa86292a1747d11d9
ruslan <rus.pro94@bk.ru>
2022-10-03 10:18:44 +0500

    .\002kolichestvochisel\

---
+++ 002kolichestvochisel/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Напишите программу,
+ которая принимает на вход число и
+  выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5
+*/
+
+Console.WriteLine("Введите число:");
+string number = Console.ReadLine();
+int count = 0;
+for (int i = 0; i < number.Length; i++)
+{
+if (int.TryParse(number[i].ToString(), out int result))
+count++;
+}
+Console.WriteLine(count);
+
