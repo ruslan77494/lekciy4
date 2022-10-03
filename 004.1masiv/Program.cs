e9c56abbc2bd6187b18a390cc35a5f471a57c1fb
ruslan <rus.pro94@bk.ru>
2022-10-03 10:58:52 +0500

    .\004.1masiv\

---
+++ 004.1masiv/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите размерность массива:");
+int demension = int.Parse(Console.ReadLine());
+Random rnd = new Random();
+int[] arr = new int[demension];
+
+for (int i = 0; i < demension; i++)
+{
+arr[i] = rnd.Next(0,2);
+}
+
+var result = string.Join(", ", arr);
+Console.WriteLine(result);
